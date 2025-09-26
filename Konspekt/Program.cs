1fd08334d646b69d18de2dc8d5e3b7a372c60d81
Opilane TTHK <opilane@tthk.ee>
2025-09-26 14:59:44 +0300

    Update Program.cs

--- Konspekt/Program.cs
+++ Konspekt/Program.cs
@@ -38,147 +38,155 @@ int kasutajavanus = int.Parse(Console.ReadLine());
 
 //}
 //2.2 mitu tingimust kasutades loogilist tehet "and"
-if (kasutajavanus > 0)
-{
-    if (kasutajavanus < 16)
-    {
-        Console.WriteLine("Kahjuks enerksi sa ei saa, oled alakas, monsterist ilma");
-    }
-    else
-    {
-        Console.WriteLine("Yippie! saad monsut osta");
-    }
+//if (kasutajavanus > 0)
+//{
+//    if (kasutajavanus < 16)
+//    {
+//        Console.WriteLine("Kahjuks enerksi sa ei saa, oled alakas, monsterist ilma");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Yippie! saad monsut osta");
+//    }
 
 
-}
-//2.3 mitu vahemikku if/else-if abil. andmetüüp double/float/decimal
-Console.WriteLine($"Siseta oma pikkus ka {nimi}");
-double kasutajaPikkus = double.Parse(Console.ReadLine());
+//}
+////2.3 mitu vahemikku if/else-if abil. andmetüüp double/float/decimal
+//Console.WriteLine($"Siseta oma pikkus ka {nimi}");
+//double kasutajaPikkus = double.Parse(Console.ReadLine());
 
 
-if (kasutajaPikkus < 1.00d)
-{
+//if (kasutajaPikkus < 1.00d)
+//{
    
-    Console.WriteLine("Oled Juntsu");
-}
+//    Console.WriteLine("Oled Juntsu");
+//}
 
- else if (kasutajaPikkus < 1.50d && kasutajaPikkus >= 1.25d)
-{
-    Console.WriteLine("Oled peaaegu allameetrimees");
-} 
+// else if (kasutajaPikkus < 1.50d && kasutajaPikkus >= 1.25d)
+//{
+//    Console.WriteLine("Oled peaaegu allameetrimees");
+//} 
 
 
-else if (kasutajaPikkus < 1.25d && kasutajaPikkus >= 1.00d)
-{
-    Console.WriteLine("OIOIOI, päkapik enam polegi, "+nimi);
-}
-else if (kasutajaPikkus < 1.75d && kasutajaPikkus >= 1.5d)
-{
-    Console.WriteLine("Oled enamasti standardpikkuses, kui mitte natuke lühike,, "+nimi);
-}
+//else if (kasutajaPikkus < 1.25d && kasutajaPikkus >= 1.00d)
+//{
+//    Console.WriteLine("OIOIOI, päkapik enam polegi, "+nimi);
+//}
+//else if (kasutajaPikkus < 1.75d && kasutajaPikkus >= 1.5d)
+//{
+//    Console.WriteLine("Oled enamasti standardpikkuses, kui mitte natuke lühike,, "+nimi);
+//}
 
-else if (kasutajaPikkus < 2.00d && kasutajaPikkus >= 1.75d)
-{
-    Console.WriteLine("Oled pikk kolge, vaata et oma suurt pead ära ei löö"+nimi);
-}
-else
-{
-    Console.WriteLine("Täielik tulnukas! Kuidas pilved välja näevad lähedalt?");
-}
-//3. kalkulaator ifi ja else-ifiga, int ja string andmetüüp
-int liidetav1 = 0;
-int liidetav2 = 0;
-Console.WriteLine("Tere, palun sisesta esimene liidetav");
-liidetav1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Tere, palun sisesta esimene liidetav");
-liidetav2 = int.Parse(Console.ReadLine());
-int liitmine = liidetav1 + liidetav2;
-int arv1 = 0;
-int arv2 = 0;
-Console.WriteLine("Tere, palun sisesta esimene arv: ");
-arv1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Tere, palun sisesta teine arv: ");
-arv2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / * ^");
-string tehtetüüp = Console.ReadLine();
-int tehe = 0;
-if (tehtetüüp == "+" )
-{
-    tehe = arv1 + arv2;
-}
-if (tehtetüüp == "-" )
-{
-    tehe = arv1 - arv2;
-}
-if (tehtetüüp == "*" )
-    tehe = arv1 * arv2;
-{ 
-if (tehtetüüp == "/" )
-{
-    tehe = arv1 / arv2;
-}
-if (tehtetüüp == "^")
-    tehe = (int)Math.Pow(arv1, arv2);
-}
+//else if (kasutajaPikkus < 2.00d && kasutajaPikkus >= 1.75d)
+//{
+//    Console.WriteLine("Oled pikk kolge, vaata et oma suurt pead ära ei löö"+nimi);
+//}
+//else
+//{
+//    Console.WriteLine("Täielik tulnukas! Kuidas pilved välja näevad lähedalt?");
+//}
+////3. kalkulaator ifi ja else-ifiga, int ja string andmetüüp
+//int liidetav1 = 0;
+//int liidetav2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene liidetav");
+//liidetav1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta esimene liidetav");
+//liidetav2 = int.Parse(Console.ReadLine());
+//int liitmine = liidetav1 + liidetav2;
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta teine arv: ");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / * ^");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
+//if (tehtetüüp == "+" )
+//{
+//    tehe = arv1 + arv2;
+//}
+//if (tehtetüüp == "-" )
+//{
+//    tehe = arv1 - arv2;
+//}
+//if (tehtetüüp == "*" )
+//    tehe = arv1 * arv2;
+//{ 
+//if (tehtetüüp == "/" )
+//{
+//    tehe = arv1 / arv2;
+//}
+//if (tehtetüüp == "^")
+//    tehe = (int)Math.Pow(arv1, arv2);
+//}
 
-Console.WriteLine($"{tehtetüüp} Tehte tulemus: {tehe}");
-//4. parool, if ja string andmetüüp
-Console.WriteLine($"Palun vabandust{ nimi}, aga programmi edasiseks tööks on parooli vaja");
-string password = Console.ReadLine();
-if (password =="simsalabim")
-{
-    Console.WriteLine("YIS, parool on õige, oled sisse lubatud");
-}
-else if (password =="saatana" || password =="1234")
-{
-    Console.WriteLine("Parool on sobimatu, see sinu parool kindlasti ei ole");
-}
-else
-{
-    Console.WriteLine("Parool on vale");
-}
-//5 värvituvastus
-Console.WriteLine("Mis on sinu lemmikvärv?");
-string favColour = Console.ReadLine();
-if (favColour == "punane")
-{
-    Console.BackgroundColor = ConsoleColor.Red;
-}
-else if (favColour == "roheline")
-{
-    Console.BackgroundColor = ConsoleColor.Green;
-}
-else if (favColour == "sinine")
-{
-    Console.BackgroundColor = ConsoleColor.Blue;
-}
-else if (favColour == "kollane")
-{
-    Console.BackgroundColor = ConsoleColor.Yellow;
-}
-else if (favColour == "valge")
-{
-    Console.BackgroundColor = ConsoleColor.White;
-}
-else if (favColour == "oranz")
-{
-    Console.WriteLine("Kahjuks ei ole oranz saadaval");
-}
-else if (favColour == "must")
-{
-    Console.BackgroundColor = ConsoleColor.Black;
-}
-else if (favColour == "lilla")
-{
-    Console.BackgroundColor = ConsoleColor.DarkMagenta;
-}
-else if (favColour == "roosa")
-{
-    Console.BackgroundColor = ConsoleColor.Magenta;
-}
+//Console.WriteLine($"{tehtetüüp} Tehte tulemus: {tehe}");
+////4. parool, if ja string andmetüüp
+//Console.WriteLine($"Palun vabandust{ nimi}, aga programmi edasiseks tööks on parooli vaja");
+//string password = Console.ReadLine();
+//if (password =="simsalabim")
+//{
+//    Console.WriteLine("YIS, parool on õige, oled sisse lubatud");
+//}
+//else if (password =="saatana" || password =="1234")
+//{
+//    Console.WriteLine("Parool on sobimatu, see sinu parool kindlasti ei ole");
+//}
+//else
+//{
+//    Console.WriteLine("Parool on vale");
+//}
+////5 värvituvastus
+//Console.WriteLine("Mis on sinu lemmikvärv?");
+//string favColour = Console.ReadLine();
+//if (favColour == "punane")
+//{
+//    Console.BackgroundColor = ConsoleColor.Red;
+//}
+//else if (favColour == "roheline")
+//{
+//    Console.BackgroundColor = ConsoleColor.Green;
+//}
+//else if (favColour == "sinine")
+//{
+//    Console.BackgroundColor = ConsoleColor.Blue;
+//}
+//else if (favColour == "kollane")
+//{
+//    Console.BackgroundColor = ConsoleColor.Yellow;
+//}
+//else if (favColour == "valge")
+//{
+//    Console.BackgroundColor = ConsoleColor.White;
+//}
+//else if (favColour == "oranz")
+//{
+//    Console.WriteLine("Kahjuks ei ole oranz saadaval");
+//}
+//else if (favColour == "must")
+//{
+//    Console.BackgroundColor = ConsoleColor.Black;
+//}
+//else if (favColour == "lilla")
+//{
+//    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+//}
+//else if (favColour == "roosa")
+//{
+//    Console.BackgroundColor = ConsoleColor.Magenta;
+//}
 
-else
+//else
+//{
+//    Console.WriteLine("Ei tea seda värvi!!!");
+//}
+//Console.WriteLine("The colour has maybe changed!!!!!");
+
+Console.WriteLine("Tere, kas sa tahad ära mõõta pappkasti või õlitünni?");
+string mõõdetav = Console.ReadLine();
+if (mõõdetav == "pappkasti")
 {
-    Console.WriteLine("Ei tea seda värvi!!!");
+    Console.WriteLine("Mis on sinu pappkasti mõõtmed?");
 }
-Console.WriteLine("The colour has maybe changed!!!!!");
+Console.ReadLine();
