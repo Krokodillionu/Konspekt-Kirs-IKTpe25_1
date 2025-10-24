6b80dc27081c5bb51eed8d8663c2907ea3dbd765
Krokodillionu <aarokirs23@gmail.com>
2025-10-24 16:03:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,9 @@
-﻿
-
-Console.WriteLine("Palun sisesta oma ostusumma");
-// Console.WriteLine prindib teksti või väärtuse järele uue rea
-int ostusumma = 0;
-// Int salvestab täisarve ilma kümnendkohtadeta
- ostusumma = int.Parse(Console.ReadLine());
-// int.Parse teisendab stringi 32-bitiliseks täisarvuks
-
- if (ostusumma > 0 && ostusumma <= 24)
-    {
-        Console.WriteLine("Sinu allahindlus on 0%");
-    }
- // if ja if else on tingimuslaused, mis võimaldavad koodi harude loomist
-    else if (ostusumma >= 25 && ostusumma <= 49)
-    {
-        Console.WriteLine("Sinu allahindlus on 5%");
-
-    }
-    else if (ostusumma >= 50 && ostusumma <= 74)
-    {
-        Console.WriteLine("Sinu allahindlus on 10%");
-    }
-    else if (ostusumma >= 75 && ostusumma <= 100)
-    {
-        Console.WriteLine("Sinu allahindlus on 20%");
-    }
-    else if (ostusumma >= 101)
-    {
-        Console.WriteLine("Sinu allahindlus on 20%");
-    }
+﻿Console.WriteLine("Tere, kas su tuba on kolmnurga, ringi või ristküliku kujuline?");
+string valik = Console.ReadLine();
+if (valik == "kolmnurk")
+{
+    Console.WriteLine("Palun sisesta oma toa esimese külje andmed (a)");
+    string külgA = Console.ReadLine();
 
 
-
+}
